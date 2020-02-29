60920b5ca2949c15852de6e7e73c41f9978945a4
Olivier ZUNTINI <olivierzuntini@MacBook-Pro-de-Olivier.local>
2020-02-29 22:21:07 +0100

    #MLua_mount_imgSD Création d'un script de montage des images des cartes SD pour Qemu et de copie d'un fichier Ajout de commentaires dans les scripts MLua_PIC et MLua_Test # SOLARECL.TXT Modification des séparateurs utilisation de "," et ":" pour améliorer la lisibilité # eclipse_OZ.lua Modification de la fonction read_script pour prise en compte des séparateurs "," et ":" Passage en variable de la version du directory et du fichier de schedule.

---
+++ MLua_mount_imgSD.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#Script de montage des images des cartes SD pour Qemu et de copie d'un fichier
+
+repQemu="/Users/olivierzuntini/Documents/Projects/qemu-eos"
+
+if [ "$1" == "" ] || [ -d "$1" ] || [ "$1" == "-h" ]; then
+    echo "usage: MLua_PIC.sh Nom_du_fichier"
+    exit
+else
+    if [ -s "$1" ]; then
+        nameScript=$1
+    else
+        echo "Fichier "$1" introuvable !"
+        exit 1
+    fi
+fi
+
+if [ -d /Volumes/EOS_DIGITAL ]; then
+    echo "Carte SD EOS_DIGITAL présente."
+    echo "Veuillez la démonter."
+    exit 1
+fi
+
+echo "Montage des images de SD : " $nameScript "."
+
+echo "-> Déplacement dans le répertoir Qemu-eos"
+cd $repQemu
+
+echo "-> Montage de la pseudo carte SD"
+./mount.sh
+
+echo "-> Copie du script " $nameScript " sur la carte 1"
+cp /Users/olivierzuntini/Documents/Projects/MagicLantern/ML_Eclipse/$nameScript /Volumes/EOS_DIGITAL/ML/scripts/
+
+echo "-> Copie du script " $nameScript " sur la carte 2"
+cp /Users/olivierzuntini/Documents/Projects/MagicLantern/ML_Eclipse/$nameScript /Volumes/EOS_DIGITA1/ML/scripts/
+
+echo "-> Fermeture des cartes 1 et 2"
+hdiutil detach "/Volumes/EOS_DIGITAL"
+hdiutil detach "/Volumes/EOS_DIGITA1"
+
+
+echo "-> Retour sur le répertoire initial"
+cd -
+
+echo "Fin du script de copie."
